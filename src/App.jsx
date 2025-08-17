f0f13eacc43b5e7c4ce034ac38dcf5b09d503d1a
rlarhdms1128 <143855484+rlarhdms1128@users.noreply.github.com>
2025-08-17 20:14:13 +0900

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,3 @@
-//import { useState } from 'react';
-//import reactLogo from './assets/react.svg';
-//import viteLogo from '/vite.svg';
-//import './App.css';
-
-//import DisplayFruits from './components/DisplayFruits'; // ✅ 고침
-
-//function App() {
-//  return (
-//    <div>
-//      <DisplayFruits />
-//    </div>
-//  );
-//}
-
-//export default App;
-
-//App.jsx
 import Filter from './components/Filter'; 
 
 function App() {
